5f23eb4f40676cabe982361a781a9926bbda74bd
spoozie-tekkz <dmjofficial5@gmail.com>
2024-05-27 20:47:31 -0400

    first commit

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+const telegramConfig = {
+    botToken: '6933200522:AAF_sOLiRvKv9q1ne16K6Xs2IcZj9ClC3rw', // Replace with your actual bot token
+    chatId: '1859591977' // Replace with your actual chat ID
+};
